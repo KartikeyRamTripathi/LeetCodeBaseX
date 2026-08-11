725ed24a105ea59bb49189fa24f9bd57e3381fc2
Kartikey Ram Tripathi <tripathikartikey883@gmail.com>
2026-08-11 14:59:37 +0530

    Add files via upload

---
+++ LeetCode0125.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); 
+        return check(s,0);
+    }
+    private boolean check(String s, int i){
+        int n = s.length();
+        if(i>=n/2){
+            return true;
+        }
+        if(s.charAt(i)!=s.charAt(n-i-1)){
+            return false;
+        }
+        return check(s,i+1);
+    }
+}
